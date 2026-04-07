e9b1af3ac7b5248eee07816871452d9be8906252
Andrej <pytpoint@gmail.com>
2026-04-07 17:46:13 +0200

    fix main logic

--- main.py
+++ main.py
@@ -290,5 +290,7 @@ def main():
             view.show_birthdays(birthdays)
         
 
-if __name__ == "__main__": 
+if __name__ == "__main__":  
+    main()
+if __name__ == "__main__":  
     main()
